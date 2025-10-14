a50963e9bda7cf5ee1d84f0f32fef4cbcbb265c0
GalloJunior <gallojunior@gmail.com>
2025-10-13 21:04:31 -0300

    Finalizada a camada de dados e configuração dos serviços

--- GCook/Models/ReceitaIngrediente.cs
+++ GCook/Models/ReceitaIngrediente.cs
@@ -1,12 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace GCook.Models;
 
-namespace GCook.Models
+[Table("ReceitaIngrediente")]
+public class ReceitaIngrediente
 {
-    public class ReceitaIngrediente
-    {
-        
-    }
+    [Key, Column(Order = 1)]
+    [Display(Name = "Receita")]
+    public int ReceitaId { get; set; }
+    [ForeignKey("ReceitaId")]
+    [Display(Name = "Receita")]
+    public Receita Receita { get; set; }
+
+    [Key, Column(Order = 2)]
+    [Display(Name = "Ingrediente")]
+    public int IngredienteId { get; set; }
+    [ForeignKey("IngredienteId")]
+    [Display(Name = "Ingrediente")]
+    public Ingrediente Ingrediente { get; set; }
+
+    [Required]
+    [StringLength(30)]
+    public string Quantidade { get; set; }
 }
